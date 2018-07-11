93ca33a6ef6011d42872df9f0195254609d7822b
Sarah GP <sarah.groff.palermo@gmail.com>
2018-07-11 18:30:24 -0400

    before I make more mistakes

--- projects/hn/src/hn/core.clj
+++ projects/hn/src/hn/core.clj
@@ -4,23 +4,45 @@
            [org.openqa.selenium.chrome ChromeDriver])
   (:gen-class))
 
+(def ^:dynamic *driver* nil)
+
 (defn split-on-space
   [str]
   (s/split str #" "))
 
-(defn format-names-count
-  [titles]
-  (let [splits (map split-on-space titles)
-        correct-cap (fn [word] (if (not= word "and") (s/capitalize word) word)) ]
-    splits
-    (map #(hash-map :name   (->> %
-                                 pop
-                                 (map correct-cap)
-                                 (s/join " " ))
-                    :number (last %))
-         splits)))
+(defn correct-cap
+  [word]
+  (if (not= word "and")
+    (s/capitalize word)
+    word))
 
-(format-names-count (list "abstract 15833" "advertisement 164" "allegorical painting 1157"))
+(defn break-up-name
+  [el]
+   (split-on-space (:name el)))
+
+(defn format-names-count
+  [elements]
+  (map
+   (fn [el]
+     (let [name-vec (break-up-name el)]
+       (assoc
+        el
+        :number (last name-vec)
+        :name (->> name-vec
+                   pop
+                   (map correct-cap)
+                   (s/join " " )))))
+    elements))
+
+(defn new-page-and-summary
+  [link]
+  (binding [*driver* (doto *driver*
+                     (.get link))]
+    (let [elements (.findElements *driver* (By/cssSelector ".dictionary-description-text"))
+          summary (map #(.getText %) elements)]
+
+      (println summary)
+      summary)))
 
 (defn print-genres-summaries
   [{:keys [titles]}]
@@ -28,18 +50,31 @@
 
 (defn scrape
   []
-  (let [driver (doto (ChromeDriver.)
+  (binding [*driver* (doto (ChromeDriver.)
                      (.get "http://www.wikiart.org/en/paintings-by-genre"))]
 
-    (let [elements (.findElements driver (By/cssSelector ".dictionaries-list .dottedItem a"))
+    (let [elements          (.findElements *driver* (By/cssSelector ".dictionaries-list .dottedItem a"))
+          titles-and-links  (map #(hash-map :name (.getText %)
+                                            :link (.getAttribute % "href" ))
+                                            elements)
+          formatted-elements (format-names-count titles-and-links)
+          elements-with-summaries (map
+                                   #(assoc % :summary (new-page-and-summary (:link %)))
+                                   formatted-elements)
 
           titles (map #(.getText %) elements)
-          links-to-genre (map #(.getAttribute % "href" ) elements)]
+          links-to-genre (map #(.getAttribute % "href" ) elements)
+          ]
 
-      ;(println titles)
-      (print-genres-summaries {:titles (format-names-count titles)})
-      #_(doseq [title titles]
-        (println title))
+      ;; basic element
+      ;; create map with formatted name and number and link
+      ;; assoc summary with map
+      ;; print everything
+
+      (println elements-with-summaries)
+      ;(new-page-and-summary driver (first links-to-genre))
+      ;(print-genres-summaries {:titles (format-names-count titles)})
+
 
       )
 
@@ -52,5 +87,5 @@
 
 
 ;; Genre: XX artists
-;; Paragraph about Genre
+;; Paragraph about Genre -> replace with featured paintings
 ;; Artists Include:
